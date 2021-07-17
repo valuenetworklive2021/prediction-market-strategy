bfadc89a4b0b015756b915c3cee98e11d1427f84
Ameesha <ameeshaagrawal12@gmail.com>
2021-07-17 15:12:54 +0530

    Added strategy in progress

---
+++ contracts/Strategy.sol
@@ -0,0 +1,77 @@
+pragma solidity 0.8.0;
+
+import "./interfaces/IPredictionMarket.sol";
+
+contract Strategy {
+    IPredictionMarket public predictionMarket;
+
+    constructor(
+        address _predictionMarket,
+        string memory _name,
+        address _trader
+    ) {
+        //check zero addresses
+        predictionMarket = IPredictionMarket(_predictionMarket);
+        //initialise above vars
+    }
+
+    function addUserFund() public payable {
+        require(msg.value > 0, "Strategy::addUserFund:SEND SOME FUNDS.");
+        User storage user = userInfo[msg.sender];
+        user.userFund += msg.value;
+        user.userBalance += msg.value;
+        userVolume += msg.value;
+        users.push(msg.sender);
+    }
+
+    function addTraderFund() public payable {
+        require(msg.value > 0, "Strategy::addTraderFund:SEND SOME FUNDS.");
+        traderFund += msg.value;
+        traderBalance += msg.value;
+    }
+
+    function removeUserFund(uint256 _amount) public {
+        User storage user = userInfo[msg.sender];
+
+        require(
+            amount > 0 && amount <= user.userBalance,
+            "Strategy::removeUserFund:INVALID AMOUNT."
+        );
+        user.userFund -= amount;
+        user.userBalance -= amount;
+        userVolume -= amount;
+
+        (msg.sender).transfer(amount);
+    }
+
+    function removeTraderFund(uint256 _amount) public {
+        require(
+            amount > 0 && amount <= traderBalance,
+            "Strategy::removeUserFund:INVALID AMOUNT."
+        );
+        traderFund -= amount;
+        traderBalance -= amount;
+
+        (msg.sender).transfer(amount);
+    }
+
+    function placeBet(
+        uint256 _conditionIndex,
+        uint8 _side,
+        uint256 _amount
+    ) public {}
+
+    function getConditionDetails(uint256 _conditionIndex)
+        public
+        view
+        returns (
+            string memory market,
+            uint256 settlementTime,
+            bool isSettled
+        )
+    {
+        (market, , , settlementTime, isSettled, , , , , ) = (
+            predictionMarket.conditions(_conditionIndex)
+        );
+    }
+}
