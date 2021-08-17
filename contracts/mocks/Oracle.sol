693d7c446bdd1df2fb4e8c5e34c7967d0ce6274e
Ameesha <ameeshaagrawal12@gmail.com>
2021-08-18 01:08:59 +0530

    Added mocks for tests

---
+++ contracts/mocks/Oracle.sol
@@ -0,0 +1,82 @@
+// File: contracts/AggregatorV3Interface.sol
+
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.0;
+
+interface IAggregatorV3Interface {
+    function decimals() external view returns (uint8);
+
+    function description() external view returns (string memory);
+
+    function version() external view returns (uint256);
+
+    // getRoundData and latestRoundData should both raise "No data present"
+    // if they do not have data to report, instead of returning unset values
+    // which could be misinterpreted as actual reported values.
+    function getRoundData(uint80 _roundId)
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
+
+    function latestRoundData()
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
+}
+
+// File: contracts/mock/Oracle.sol
+
+// File: @chainlink/contracts/src/v0.7/interfaces/AggregatorV3Interface.sol
+
+// File: contracts/PriceOracle.sol
+contract Oracle is IAggregatorV3Interface {
+    uint8 public override decimals = 8;
+    string public override description = "BTC/USD";
+    uint256 public override version = 1;
+
+    // getRoundData and latestRoundData should both raise "No data present"
+    // if they do not have data to report, instead of returning unset values
+    // which could be misinterpreted as actual reported values.
+    function getRoundData(uint80 _roundId)
+        external
+        view
+        override
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        )
+    {
+        return (_roundId, 1, 1, block.timestamp, 1);
+    }
+
+    function latestRoundData()
+        external
+        view
+        override
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        )
+    {
+        return (1, 6000000000000, 1, block.timestamp, 1);
+    }
+}
